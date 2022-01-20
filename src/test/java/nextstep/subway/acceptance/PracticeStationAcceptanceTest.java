0a2860a291c14464cced93da1f607435918eb800
soosue <yunsoo0510@daum.net>
2022-01-20 16:33:12 +0900

    [실습] 인수 테스트 리팩터링: 메서드 분리, 한글 메서드명 리팩터링

--- src/test/java/nextstep/subway/acceptance/PracticeStationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PracticeStationAcceptanceTest.java
@@ -34,58 +34,75 @@ public class PracticeStationAcceptanceTest {
         databaseCleanup.execute();
     }
 
+    /**
+     * Scenario: 지하철 역을 생성한다.
+     * When: 지하철 역을 생성 요청한다.
+     * Then: 지하철 역이 생성된다.
+     */
     @Test
     void 지하철역을_생성() {
         // given
-        Map<String, String> params = new HashMap<>();
-        params.put("name", "판교역");
+        final String 판교역 = "판교역";
 
         // when
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
+        ExtractableResponse<Response> response = 지하철_역_등록_요청(판교역);
+
+        // then
+        지하철_역_등록됨(response);
+    }
+
+    /**
+     * Scenario: 지하철역 목록을 조회한다.
+     * Given: 지하철 역이 이미 등록되어 있다.
+     * When: 모든 지하철 역 목록을 요청한다.
+     * Then: 모든 지하철 역 목록이 반환된다.
+     */
+    @Test
+    void 지하철역_목록을_조회() {
+        //given
+        final String 강남역 = "강남역";
+        지하철_역_등록되어_있음(강남역);
+
+        final String 판교역 = "판교역";
+        지하철_역_등록되어_있음(판교역);
+
+        // when
+        ExtractableResponse<Response> response = 지하철_역_목록_조회_요청();
+
+        지하철_역_목록_조회됨(강남역, 판교역, response);
+    }
+
+    private ExtractableResponse<Response> 지하철_역_등록_요청(String name) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+
+        return RestAssured.given().log().all()
                 .body(params)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
                 .post("/stations")
                 .then().log().all()
                 .extract();
+    }
 
-        // then
+    private void 지하철_역_등록됨(ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         assertThat(response.header("Location")).isNotBlank();
     }
 
-    @Test
-    void 지하철역_목록을_조회() {
-        //given
-        Map<String, String> params = new HashMap<>();
-        final String 강남역 = "강남역";
-        params.put("name", 강남역);
-        ExtractableResponse<Response> createResponse1 = RestAssured.given().log().all()
-                .body(params)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .post("/stations")
-                .then().log().all()
-                .extract();
+    private void 지하철_역_등록되어_있음(String name) {
+        ExtractableResponse<Response> createResponse = 지하철_역_등록_요청(name);
+    }
 
-        Map<String, String> params2 = new HashMap<>();
-        final String 판교역 = "판교역";
-        params2.put("name", 판교역);
-        ExtractableResponse<Response> createResponse2 = RestAssured.given().log().all()
-                .body(params2)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .post("/stations")
-                .then().log().all()
-                .extract();
-
-        // when
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
+    private ExtractableResponse<Response> 지하철_역_목록_조회_요청() {
+        return RestAssured.given().log().all()
                 .when()
                 .get("/stations")
                 .then().log().all()
                 .extract();
+    }
 
+    private void 지하철_역_목록_조회됨(String 강남역, String 판교역, ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         List<String> stationNames = response.jsonPath().getList("name");
         assertThat(stationNames).contains(강남역, 판교역);
