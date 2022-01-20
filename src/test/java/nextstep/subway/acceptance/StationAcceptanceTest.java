9302b094495e8f9f1c64c72179c9eceab6122119
soosue <yunsoo0510@daum.net>
2022-01-20 17:02:31 +0900

    [실습] 지하철역 관리 기능: 인수 조건 업데이트

--- src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
@@ -17,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("지하철역 관리 기능")
 class StationAcceptanceTest extends AcceptanceTest {
     /**
+     * Scenario: 지하철역 생성
      * When 지하철역 생성을 요청 하면
      * Then 지하철역 생성이 성공한다.
      */
@@ -42,10 +43,11 @@ class StationAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * Given 지하철역 생성을 요청 하고
-     * Given 새로운 지하철역 생성을 요청 하고
+     * Scenario: 지하철역 목록 조회
+     * Given 강남역 지하철역 생성을 요청 하고
+     * Given 역삼역 지하철역 생성을 요청 하고
      * When 지하철역 목록 조회를 요청 하면
-     * Then 두 지하철역이 포함된 지하철역 목록을 응답받는다
+     * Then 강남역, 역삼역이 포함된 지하철역 목록을 응답받는다
      */
     @DisplayName("지하철역 목록 조회")
     @Test
@@ -86,9 +88,10 @@ class StationAcceptanceTest extends AcceptanceTest {
     }
 
     /**
-     * Given 지하철역 생성을 요청 하고
-     * When 생성한 지하철역 삭제를 요청 하면
-     * Then 생성한 지하철역 삭제가 성공한다.
+     * Scenario: 지하철역 삭제
+     * Given 강남역 지하철역 생성을 요청 하고
+     * When 강남역 지하철역 삭제를 요청 하면
+     * Then 강남역 지하철역 삭제가 성공한다.
      */
     @DisplayName("지하철역 삭제")
     @Test
