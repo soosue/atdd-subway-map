ca55d7ebccfdf60b81245ae970c8d137fda5dbd5
soosue <yunsoo0510@daum.net>
2022-01-20 15:46:12 +0900

    [실습] 지하철역을 생성 인수 테스트 만들기

--- src/test/java/nextstep/subway/acceptance/PracticeStationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/PracticeStationAcceptanceTest.java
@@ -1,10 +1,20 @@
 package nextstep.subway.acceptance;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("지하철역 인수테스트 실습")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -15,5 +25,25 @@ public class PracticeStationAcceptanceTest {
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
+    }
+
+    @Test
+    void 지하철역을_생성() {
+        // given
+        Map<String, String> params = new HashMap<>();
+        params.put("name", "판교역");
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/stations")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("Location")).isNotBlank();
     }
 }
