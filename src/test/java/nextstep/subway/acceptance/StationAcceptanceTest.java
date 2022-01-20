6315a90b383f62f508d329d7bf71273aafd7b961
soosue <yunsoo0510@daum.net>
2022-01-20 17:14:41 +0900

    [실습] 지하철역 관리 기능: 인수 조건을 검증하는 인수 테스트 작성하기

--- src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
+++ src/test/java/nextstep/subway/acceptance/StationAcceptanceTest.java
@@ -24,6 +24,14 @@ class StationAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철역 생성")
     @Test
     void createStation() {
+        // given
+        String 강남역 = "강남역";
+
+        // when
+        ExtractableResponse<Response> response = 지하철역_생성_요청(강남역);
+
+        // then
+        지하철역_생성됨(response);
     }
 
     /**
@@ -36,6 +44,18 @@ class StationAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철역 목록 조회")
     @Test
     void getStations() {
+        // given
+        String 강남역 = "강남역";
+        지하철역_이미_등록되어_있음(강남역);
+
+        String 역삼역 = "역삼역";
+        지하철역_이미_등록되어_있음(역삼역);
+
+        // when
+        ExtractableResponse<Response> response = 지하철역_목록_조회_요청();
+
+        // then
+        지하철역_목록_조회됨(강남역, 역삼역, response);
     }
 
     /**
@@ -47,5 +67,62 @@ class StationAcceptanceTest extends AcceptanceTest {
     @DisplayName("지하철역 삭제")
     @Test
     void deleteStation() {
+        // given
+        String 강남역 = "강남역";
+        ExtractableResponse<Response> createResponse = 지하철역_생성_요청(강남역);
+        String uri = createResponse.header("Location");
+
+        // when
+        ExtractableResponse<Response> response = 지하철역_삭제_요청(uri);
+
+        // then
+        지하철역_삭제됨(response);
+    }
+
+    private ExtractableResponse<Response> 지하철역_생성_요청(String name) {
+        Map<String, String> params = new HashMap<>();
+        params.put("name", name);
+
+        return RestAssured.given().log().all()
+                .body(params)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> 지하철역_목록_조회_요청() {
+        return RestAssured.given().log().all()
+                .when()
+                .get("/stations")
+                .then().log().all()
+                .extract();
+    }
+
+    private ExtractableResponse<Response> 지하철역_삭제_요청(String uri) {
+        return RestAssured.given().log().all()
+                .when()
+                .delete(uri)
+                .then().log().all()
+                .extract();
+    }
+
+    private void 지하철역_이미_등록되어_있음(String name) {
+        ExtractableResponse<Response> createResponse = 지하철역_생성_요청(name);
+    }
+
+    private void 지하철역_생성됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("Location")).isNotBlank();
+    }
+
+    private void 지하철역_목록_조회됨(String 강남역, String 역삼역, ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("name")).contains(강남역, 역삼역);
+    }
+
+    private void 지하철역_삭제됨(ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 }
